5ab67f8abe69ed55928952a59ccb05ea073c88b4
Oscar Stevens <oscar_stevens@icloud.com>
2020-12-29 00:34:49 +0000

    Format things

--- web_control.py
+++ web_control.py
@@ -10,14 +10,12 @@ serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        respond_json(
-            self, switch({lambda _: "default": lambda _: "hello"}, self.path))
+        respond_json(self, switch({lambda _: "default": lambda _: "hello"}, self.path))
 
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
-    print(GREEN + ("Server started http://%s:%s" % (hostName, serverPort)) +
-          RESET)
+    print(GREEN + ("Server started http://%s:%s" % (hostName, serverPort)) + RESET)
 
     try:
         webServer.serve_forever()
