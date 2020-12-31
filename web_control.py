52a397db0707ee7984ea36727a7eb40e3566ee36
Oscar Stevens <oscar_stevens@icloud.com>
2020-12-31 23:03:55 +0000

    Get things working better

--- web_control.py
+++ web_control.py
@@ -4,13 +4,25 @@ import time
 
 from helpers import switch, GREEN, RESET
 
-hostName = "localhost"
+hostName = "raspberrypi.local"
 serverPort = 8080
 
 
 class MyServer(BaseHTTPRequestHandler):
+    speed: int = 0
+    angle: int = 90
+
     def do_GET(self):
-        respond_json(self, switch({lambda _: "default": lambda _: "hello"}, self.path))
+        respond_json(
+            self,
+            switch(
+                {
+                    lambda _: "default": lambda _: """{ "command": "status", "speed": "%s", "angle": "%s" }"""
+                    % (self.speed, self.angle)
+                },
+                self.path,
+            ),
+        )
 
 
 if __name__ == "__main__":
