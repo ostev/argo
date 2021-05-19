848fb6b9ceab0dacc23d4b363b699bbb29073942
Oscar Stevens <oscar_stevens@icloud.com>
2021-05-20 09:17:21 +1000

    Create a new Controller class

--- Controller.py
+++ Controller.py
@@ -2,24 +2,24 @@ from Gamepad import Gamepad
 
 
 class Controller(Gamepad):
-    def __init__(self, joystickNumber=0):
-        Gamepad.__init__(self, joystickNumber)
-
-        self.buttonNames = {
-            9: "RT",
-            7: "RB",
-            8: "LT",
-            6: "LB",
-            13: "LS_BTN",
-            14: "RS_BTN",
-            4: "Y",
-            1: "B",
-            3: "X",
-            0: "A",
-            10: "BACK",
-            11: "START",
+    def __init__(self):
+        self.names = {
+            "BTN_TR2": "RT",
+            "BTN_TR": "RB",
+            "BTN_TL2": "LT",
+            "BTN_TL": "LB",
+            "BTN_THUMBL": "LS_BTN",
+            "BTN_THUMBR": "RS_BTN",
+            "BTN_WEST": "Y",
+            "BTN_EAST": "B",
+            "BTN_SOUTH": "A",
+            "BTN_NORTH": "X",
+            "BTN_SELECT": "BACK",
+            "BTN_START": "START",
+            "ABS_X": "LS_X",
+            "ABS_Y": "LS_Y",
+            "ABS_Z": "RS_X",
+            "ABS_RZ": "RS_Y"
         }
 
-        self.axisNames = {1: "LS_Y", 0: "LS_X", 3: "RS_Y", 2: "RS_X"}
-
-        self._setupReverseMaps()
+        Gamepad.__init__(self)
