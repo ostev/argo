ce6daaf3e69db39f6f11786f8a71066bc0cad98a
Oscar Stevens <oscar_stevens@icloud.com>
2021-06-19 12:02:23 +1000

    Decrease min pivot

--- parts.py
+++ parts.py
@@ -341,7 +341,7 @@ class BrickPiOneWheelDriver(Driver):
         self.motor.run(adjusted_throttle)
 
     def min_pivot(self):
-        self.pivot.angle = -85
+        self.pivot.angle = -80
 
     def max_pivot(self):
         self.pivot.angle = 80
